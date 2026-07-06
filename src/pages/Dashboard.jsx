31a5e3be9c6554f2836c95a5617f91dd735060a3
rizafrivaldi <rizafrivaldi@gmail.com>
2026-07-06 23:26:06 +0700

    debug

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -211,6 +211,10 @@ function Dashboard() {
     tasksPerPage,
   );
 
+  useEffect(() => {
+    setCurrentPage(1);
+  }, [search, filterStatus]);
+
   return (
     <div className="min-h-screen bg-slate-100 p-8">
       <div className="flex justify-between items-center mb-8">
